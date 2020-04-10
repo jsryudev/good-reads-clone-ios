1d66aa40e172839495ff412358a0f5b41b0437c7
JunSang Ryu <jsryu.dev@gmail.com>
2020-04-10 20:45:24 +0900

    Main.storyboard 제거

--- Good Reads/Sources/AppDelegate.swift
+++ Good Reads/Sources/AppDelegate.swift
@@ -15,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    let window = UIWindow(frame: UIScreen.main.bounds)
+    window.makeKeyAndVisible()
+    
+    let viewController = ViewController()
+    window.rootViewController = viewController
+    
+    self.window = window
+    
     return true
   }
 }
