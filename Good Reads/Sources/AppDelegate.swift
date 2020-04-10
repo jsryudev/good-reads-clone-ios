05aadbb8b18235a7d272d59d641ad13043de466c
JunSang Ryu <jsryu.dev@gmail.com>
2020-04-10 16:35:57 +0900

    프로젝트 구성

---
+++ Good Reads/Sources/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Good Reads
+//
+//  Created by 유준상 on 2020/04/10.
+//  Copyright © 2020 JunSang Ryu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
---
+++ Good Reads/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Good Reads
+//
+//  Created by 유준상 on 2020/04/10.
+//  Copyright © 2020 JunSang Ryu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
